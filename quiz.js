314acc389395f1b6849414119a98b867e27ea97a
Jonny Mac <soto.jonathan6@gmail.com>
2020-06-22 10:19:15 -0400

    added Ionic CDN + added inital logic to the quiz
    First use of  Ionic library.
    Added basic logic of the quiz.
    currently doesn't throw errors for wrong answers. Throws error after the last question is answered correctly

--- quiz.js
+++ quiz.js
@@ -1,1 +1,67 @@
+var score = 0;
+var currentQuestion = 0;
+const questionArea = document.getElementById('question-placement');
+const firstAnswer = document.getElementById('first');
+const secondAnswer = document.getElementById('second');
+const thirdAnswer = document.getElementById('third');
 
+
+var myQuestions= [
+    {
+        question: "what's my name?",
+        answers: {
+            a: 'Jonny',
+            b: 'Fred',
+            c: 'Manny'
+        },
+        correct_answer: 'A'
+    },
+    {
+        question: "what's my dog's name?",
+        answers: {
+            a: 'Azula',
+            b: 'Fred',
+            c: 'Sis'
+        },
+        correct_answer: 'A'
+    },
+    {
+        question: "what's my favorite baseball team?",
+        answers: {
+            a: 'Yankees',
+            b: 'Dodgers',
+            c: 'Pirates'
+    },
+        correct_answer: 'B'
+    },{
+        question: "What is the name of my favorite band?",
+        answers: {
+            a: 'Paramore',
+            b: 'Ballyhoo!',
+            c: 'Sublime'
+    },
+        correct_answer: 'C'
+    }
+]
+
+
+function check_ans(val) { 
+    console.log(val);
+    for (var i = currentQuestion; i < myQuestions.length; i++){
+        
+        if(val == myQuestions[i].correct_answer){
+            score += 1;
+            console.log("you are correct!", score);
+            
+            //document.getElementsByName("ans_btn")[0].color = rbg(0,0,0);
+        }
+        questionArea.innerHTML = myQuestions[i + 1].question;
+        firstAnswer.innerHTML = myQuestions[i + 1].answers.a;
+        secondAnswer.innerHTML = myQuestions[i + 1].answers.b;
+        thirdAnswer.innerHTML = myQuestions[i + 1].answers.c;
+        currentQuestion += 1;
+        return
+    }
+}
+
+//for(var i =0; i < myQustions.length)
